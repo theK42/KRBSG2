04c32ff7b6822f32f37da19ba5b616683119028b
Kelson <theK42@gmail.com>
2024-01-06 15:57:23 -0800

    Minor control changes for the boys.

--- playerShip.lua
+++ playerShip.lua
@@ -5,8 +5,12 @@ local playerShip = krbsg.wrapPlayerShip(...);
 
 move = input.setOnCombinedAxisTilt("primary", 
 	function(tilt)
-		tilt.x = tilt.x * 2;
-		tilt.y = tilt.y * 2;
+		tilt.x = tilt.x * 4;
+		if (tilt.y < 0) then
+			tilt.y = tilt.y * 2;
+		else
+			tilt.y = tilt.y * 4;
+		end
 		playerShip:move(tilt);
 	end,
 ...);
